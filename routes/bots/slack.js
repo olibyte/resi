f471b9671c5a22bb9dcde49a19f555b88196579f
Oliver <ocben1@gmail.com>
2020-03-11 13:36:11 +1100

    implememnt conversation service

--- routes/bots/slack.js
+++ routes/bots/slack.js
@@ -17,22 +17,7 @@ module.exports = (params) => {
 
   router.use('/events', slackEvents.requestListener());
 
-  async function handleMention(event) {
-    const sessionId = createSessionId(event.channel, event.user, event.thread_ts || event.ts);
-    let session = sessionService.get(sessionId);
-
-    if(!session) {
-      session = sessionService.create(sessionId);
-
-      session.context = {
-        slack: {
-          channel: event.channel,
-          user: event.user,
-          thread_ts: event.thread_ts || event.ts,
-        },
-      };
-    }
-    
+  async function processEvent(session, event) {
     const mention = /<@[A-Z0-9]+>/;
     const eventText = event.text.replace(mention, '').trim();
 
@@ -62,7 +47,36 @@ module.exports = (params) => {
     });
   }
 
+
+
+  async function handleMention(event) {
+    const sessionId = createSessionId(event.channel, event.user, event.thread_ts || event.ts);
+    let session = sessionService.get(sessionId);
+
+    if(!session) {
+      session = sessionService.create(sessionId);
+
+      session.context = {
+        slack: {
+          channel: event.channel,
+          user: event.user,
+          thread_ts: event.thread_ts || event.ts,
+        },
+      };
+    }
+    return processEvent(session, event);
+  }
+
+  async function handleMessage(event) {
+    const sessionId = createSessionId(event.channel, event.user, event.thread_ts || event.ts);
+    const session = sessionService.get(sessionId);
+
+    return !session ? false : processEvent(session,event);
+  }
+    
+  
   slackEvents.on('app_mention', handleMention);
+  slackEvents.on('message', handleMessage);
 
   return router;
 };
---
+++ services/ConversationService.js
@@ -0,0 +1,42 @@
+class ConversationService {
+    static async run(witService, text, context) {
+        if (!context.conversation) {
+            context.conversation = {
+                entities: {},
+                followUp: '',
+                complete: false,
+                exit: false,
+            };
+        }
+        const entities = await witService.query(text);
+        context.conversation.entities = { ...context.conversation.entities, ...entities };
+
+        if (context.conversation.entities.intent === 'reservation') {
+            return ConversationService.intentReservation(context);
+        }
+        context.conversation.followUp = 'Could you rephrase that?';
+        return context;
+    }
+
+    static intentReservation(context) {
+        const { conversation } = context;
+        const { entities } = conversation;
+
+        if(!entities.reservationDateTime) {
+            conversation.followUp = 'For when would you like to make a reservation?';
+            return context;
+        }
+        if(!entities.numberOfGuests) {
+            conversation.followUp = 'For how many people?';
+            return context;
+        }
+        if(!entities.customerName) {
+            conversation.followUp = 'Would you tell me your name please?';
+            return context;
+        }
+        conversation.complete = true;
+        return context;
+    }
+}
+
+module.exports = ConversationService;
